277d706a2f35b048cf35c1d8f02a8e985057d465
Sai Darshan <saidar9632@gmail.com>
2024-12-29 02:21:58 +0530

    upload

--- sentiment-portal/src/axiosConfig.js
+++ sentiment-portal/src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://your-production-domain.com'; // Replace with your production domain
+const API_BASE_URL = 'https://hanabi-2.onrender.com/'; // Replace with your production domain
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
